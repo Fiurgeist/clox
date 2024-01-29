d0452899c1503d4ee3c04948c8612d325cbab7c1
fiurgeist <fiurgeist@gmail.com>
2024-01-29 20:13:30 +0100

    add stack; add opcodes for negate and binary ops for numbers

--- src/vm.c
+++ src/vm.c
@@ -7,17 +7,46 @@
 // TODO: remove global var and explicitly take a VM pointer and pass it around
 VM vm;
 
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
 void initVM() {
+  resetStack();
 }
 
 void freeVM() {
 }
 
+static void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+static Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(operator) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a operator b); \
+    } while (false)
+
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; ++slot) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
@@ -26,15 +55,22 @@ static InterpretResult run() {
     {
     case OP_CONSTANT:
       Value constant = READ_CONSTANT();
-      printValue(constant);
-      printf("\n");
+      push(constant);
       break;
+    case OP_ADD:      BINARY_OP(+); break;
+    case OP_SUBTRACT: BINARY_OP(-); break;
+    case OP_MULTIPLY: BINARY_OP(*); break;
+    case OP_DIVIDE:   BINARY_OP(/); break;
+    case OP_NEGATE:   push(-pop()); break;
     case OP_RETURN:
+      printValue(pop());
+      printf("\n");
       return INTERPRET_OK;
     default:
       return INTERPRET_COMPILE_ERROR;
     }
   }
+#undef BINARY_OP
 #undef READ_CONSTANT
 #undef READ_BYTE
 }
