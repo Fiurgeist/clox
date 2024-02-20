e29294c613ee619d0926b07bd7cadb347739e237
fiurgeist <fiurgeist@gmail.com>
2024-02-20 22:16:21 +0100

    add local variables with lexical scoping

--- src/vm.c
+++ src/vm.c
@@ -111,6 +111,11 @@ static InterpretResult run() {
     case OP_TRUE: push(BOOL_VAL(true)); break;
     case OP_FALSE: push(BOOL_VAL(false)); break;
     case OP_POP: pop(); break;
+    case OP_GET_LOCAL: {
+      uint8_t slot = READ_BYTE();
+      push(vm.stack[slot]);
+      break;
+    }
     case OP_GET_GLOBAL: {
       ObjString *name = READ_STRING();
       Value value;
@@ -122,7 +127,6 @@ static InterpretResult run() {
       break;
     }
     case OP_DEFINE_GLOBAL: {
-      printf("OP_DEFINE_GLOBAL\n");
       ObjString *name = READ_STRING();
       tableSet(&vm.globals, name, peek(0));
       pop();
