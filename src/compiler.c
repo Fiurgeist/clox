ac3dd796219495f315b44f615648d78a33edbe78
fiurgeist <fiurgeist@gmail.com>
2024-03-05 23:09:12 +0100

    add closures and resolve captured variables

--- src/compiler.c
+++ src/compiler.c
@@ -93,7 +93,13 @@ typedef struct {
 typedef struct {
   Token name;
   int depth;
+  bool isCaptured;
 } Local;
+
+typedef struct {
+  uint8_t index;
+  bool isLocal;
+} Upvalue;
 
 typedef enum {
   TYPE_FUNCTION,
@@ -106,6 +112,7 @@ typedef struct Compiler {
   FunctionType type;
   Local locals[UINT8_COUNT];
   int localCount;
+  Upvalue upvalues[UINT8_COUNT];
   int scopeDepth;
   int breakJump;
   bool inLoop;
@@ -134,11 +141,11 @@ static void errorAt(Token *token, const char *msg) {
 }
 
 static void error(const char *msg) {
-  errorAt(&parser.current, msg);
+  errorAt(&parser.previous, msg);
 }
 
 static void errorAtCurrent(const char *msg) {
-  errorAt(&parser.previous, msg);
+  errorAt(&parser.current, msg);
 }
 
 static void advance() {
@@ -252,6 +259,7 @@ static void initCompiler(Compiler *compiler, FunctionType type) {
 
   Local *local = &current->locals[current->localCount++];
   local->depth = 0;
+  local->isCaptured = false;
   local->name.start = "";
   local->name.length = 0;
 }
@@ -279,7 +287,11 @@ static void endScope() {
   while (current->localCount > 0 &&
     current->locals[current->localCount - 1].depth > current->scopeDepth
   ) {
-    emitByte(OP_POP);
+    if (current->locals[current->localCount - 1].isCaptured) {
+      emitByte(OP_CLOSE_UPVALUE);
+    } else {
+      emitByte(OP_POP);
+    }
     current->localCount--;
   }
 }
@@ -308,7 +320,7 @@ static void binary(bool canAssign) {
   case TOKEN_MINUS:         emitByte(OP_SUBTRACT); break;
   case TOKEN_STAR:          emitByte(OP_MULTIPLY); break;
   case TOKEN_SLASH:         emitByte(OP_DIVIDE); break;
-  default: errorAtCurrent("Invalid binary operator");
+  default: error("Preceded by invalid binary operator");
   }
 }
 
@@ -337,7 +349,7 @@ static void literal(bool canAssign) {
   case TOKEN_FALSE: emitByte(OP_FALSE); break;
   case TOKEN_NIL: emitByte(OP_NIL); break;
   case TOKEN_TRUE: emitByte(OP_TRUE); break;
-  default: errorAtCurrent("Invalid literal");
+  default: error("Invalid literal");
   }
 }
 
@@ -378,6 +390,43 @@ static int resolveLocal(Compiler *compiler, Token *name) {
   return -1;
 }
 
+static int addUpvalue(Compiler *compiler, uint8_t index, bool isLocal) {
+  int upvalueCount = compiler->function->upvalueCount;
+
+  for (int i = 0; i < upvalueCount; ++i) {
+    Upvalue *upvalue = &compiler->upvalues[i];
+    if (upvalue->index == index && upvalue->isLocal == isLocal) {
+      return i;
+    }
+  }
+
+  if (upvalueCount == UINT8_COUNT) {
+    error("Too many closure variables in function");
+    return 0;
+  }
+
+  compiler->upvalues[upvalueCount].isLocal = isLocal;
+  compiler->upvalues[upvalueCount].index = index;
+  return compiler->function->upvalueCount++;
+}
+
+static int resolveUpvalue(Compiler *compiler, Token *name) {
+  if (compiler->enclosing == NULL) return -1;
+
+  int local = resolveLocal(compiler->enclosing, name);
+  if (local != -1) {
+    compiler->enclosing->locals[local].isCaptured = true;
+    return addUpvalue(compiler, (uint8_t)local, true);
+  }
+
+  int upvalue = resolveUpvalue(compiler->enclosing, name);
+  if (upvalue != -1) {
+    return addUpvalue(compiler, (uint8_t)upvalue, false);
+  }
+
+  return -1;
+}
+
 static void addLocal(Token name) {
   if (current->localCount == UINT8_COUNT) {
     error("Too many local variables in function");
@@ -387,6 +436,7 @@ static void addLocal(Token name) {
   Local *local = &current->locals[current->localCount++];
   local->name = name;
   local->depth = -1; // uninitialized state
+  local->isCaptured = false;
 }
 
 static void declareVariable() {
@@ -437,6 +487,9 @@ static void namedVariable(Token name, bool canAssign) {
   if (arg != -1) {
     getOp = OP_GET_LOCAL;
     setOp = OP_SET_LOCAL;
+  } else if ((arg = resolveUpvalue(current, &name)) != -1) {
+    getOp = OP_GET_UPVALUE;
+    setOp = OP_SET_UPVALUE;
   } else {
     arg = identifierConstant(&name);
     getOp = OP_GET_GLOBAL;
@@ -463,7 +516,7 @@ static void unary(bool canAssign) {
   switch (operatorType) {
   case TOKEN_BANG: emitByte(OP_NOT); break;
   case TOKEN_MINUS: emitByte(OP_NEGATE); break;
-  default: errorAtCurrent("Invalid unary operator");
+  default: error("Preceded by invalid unary operator");
   }
 }
 
@@ -501,7 +554,12 @@ static void function(FunctionType type) {
   block();
 
   ObjFunction *function = endCompiler();
-  emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+  emitBytes(OP_CLOSURE, makeConstant(OBJ_VAL(function)));
+
+  for (int i = 0; i < function->upvalueCount; ++i) {
+    emitByte(compiler.upvalues[i].isLocal ? 1 : 0);
+    emitByte(compiler.upvalues[i].index);
+  }
 }
 
 static void funDeclaration() {
